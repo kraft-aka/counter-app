eb6c308774aa1dbed069af20ff212526344dcb7b
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-05-17 21:02:44 +0200

    useEffect hook

--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Counter() {
   let [count, setCount] = useState(0);
+  let [ color, setColor ] = useState('');
 
   function handleClick() {
     setCount(count+1);
   }
+
+  function handleColorChange() {
+    setColor(
+
+    )
+  }
+
+  useEffect(()=>{
+    document.title = count
+  });
 
   
 
@@ -15,6 +26,8 @@ function Counter() {
         Click Me to Increase The Count...
       </button>
       <h1>{count}</h1>
+      <button>Change Color</button>
+      <p>{color}</p>
     </React.Fragment>
   );
 }
