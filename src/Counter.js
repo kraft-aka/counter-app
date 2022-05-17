1e4ffc2a3d6e9363c740211a6060d07ff3789c9f
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-05-17 21:35:55 +0200

    add a button

--- src/Counter.js
+++ src/Counter.js
@@ -1,33 +1,46 @@
 import React, { useState, useEffect } from "react";
 
+let body = document.body.style.backgroundColor;
+console.log(body);
+
 function Counter() {
   let [count, setCount] = useState(0);
-  let [ color, setColor ] = useState('');
+  let [color, setColor] = useState("");
 
   function handleClick() {
-    setCount(count+1);
+    setCount(count + 1);
   }
 
-  function handleColorChange() {
-    setColor(
-
-    )
+  function handleChange() {
+    setCount(count - 1);
   }
 
-  useEffect(()=>{
-    document.title = count
+  function generateRandomColor() {
+    let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    return randomColor;
+  }
+
+  useEffect(() => {
+    document.title = count;
   });
 
-  
+  useEffect(() => {
+    setColor(generateRandomColor());
+  }, [count]);
 
   return (
     <React.Fragment>
       <button onClick={handleClick}>
-        Click Me to Increase The Count...
+        Click Me to Increase The Count and Generate a HEX Color
       </button>
       <h1>{count}</h1>
-      <button>Change Color</button>
-      <p>{color}</p>
+      <h2 style={{ color: color }}>
+        {" "}
+        <strong>{color}</strong>{" "}
+      </h2>
+      <button onClick={handleChange}>
+        Click this Button to descrease The Count
+      </button>
     </React.Fragment>
   );
 }
